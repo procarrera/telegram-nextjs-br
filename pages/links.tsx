6af16ef09f8265e4bec5a614accdde09fbaf1f75
Eduardo Oliveira <eduardo@eter.ppg.br>
2021-03-02 20:11:49 -0300

    New sorting

--- pages/links.tsx
+++ pages/links.tsx
@@ -1,4 +1,4 @@
-import { ChevronDownIcon } from "@chakra-ui/icons";
+import { useState } from "react";
 import {
   Box,
   Heading,
@@ -11,6 +11,8 @@ import {
   ListItem,
   Link as ChakraLink,
   Grid,
+  Switch,
+  Stack,
 } from "@chakra-ui/react";
 import Headroom from 'react-headroom'
 import Link from 'next/link'
@@ -24,26 +26,84 @@ const section = {
 }
 
 const alfabeto = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-
-
+const categorias = ["Cursos", "Tuts", "Docs", "Outros"]
 
 export default function Links() {
+  const [order, setOrder] = useState(false);
+  const [type, setType] = useState(false);
+  console.log(order)
+  function sortAlfa() {
+    if (order === false) {
+      return sortCats().sort()
+    } else {
+      return sortCats().reverse()
+    }
+  }
+  function sortCats() {
+    if (type === false) {
+      return alfabeto
+    } else {
+      return categorias
+    }
+  }
+
+  const CardLink = ({ cmd, link }) => {
+    return (
+      <ListItem
+        role="group"
+        p={2}
+        borderRadius="lg"
+        cursor="pointer"
+        transition="all .2s ease-in-out"
+        _hover={{
+          bgColor: 'accent.50'
+        }}
+      >
+        <Code>{cmd}</Code> - <ChakraLink
+          fontFamily="body"
+          fontSize="sm"
+          color="indigoBlue.700"
+          fontWeight="normal"
+          _groupHover={{ 
+            textDecor: 'none',
+            _after: {
+              w: '100%'
+            }
+          }}
+        >{link}</ChakraLink>
+      </ListItem>
+    )
+  }
+
   return (
     <>
       <Box as="section" minH={section.minH} py={section.py}>
         <Container>
           <Box color="orange.600" textStyle="h1">Links Úteis</Box>
           <Box textStyle="paragraph" mb={6}>
-            Links catalogados por ordem alfabética.
+            Links catalogados por {!type ? "ordem alfabética" : "categorias"}{order && !type && " reversa"}.
           </Box>
         </Container>
         <Headroom>
           <Box bgColor="white.50">
             <Container>
+              <Stack>
+                <Text as="span">Ordem</Text>
+                <HStack>
+                  <Text as="span" textTransform="uppercase">Alfabética</Text>
+                  <Switch onChange={() => { setType(!type) }} size="sm" />
+                  <Text as="span" textTransform="uppercase">Categorias</Text>
+                </HStack>
+                <HStack>
+                  <Text as="span" textTransform="uppercase">Desc</Text>
+                  <Switch onChange={() => { setOrder(!order) }} size="sm" />
+                  <Text as="span" textTransform="uppercase">Asc</Text>
+                </HStack>
+              </Stack>
               <HStack w="100%" display={['none', , 'block']} className="index" pb={12}>
-                {alfabeto.map((letter, i) => {
+                {sortAlfa().map((item, i) => {
                   return (
-                    <Link href={`#${letter}`}>
+                    <Link href={`#${item}`}>
                       <Text
                         as="span"
                         key={i}
@@ -66,7 +126,7 @@ export default function Links() {
                           }
                         }}
                       >
-                        {letter}
+                        {item}
                       </Text>
                     </Link>
                   )
@@ -77,9 +137,9 @@ export default function Links() {
         </Headroom>
         <Container>
           <VStack w="100%" display={['block', , 'none']} className="index" pb={12}>
-            {alfabeto.map((letter, i) => {
+            {sortAlfa().map((item, i) => {
               return (
-                <Link href={`#${letter}`}>
+                <Link href={`#${item}`}>
                   <Text
                     as="span"
                     key={i}
@@ -102,139 +162,47 @@ export default function Links() {
                       }
                     }}
                   >
-                    {letter}
+                    {item}
                   </Text>
                 </Link>
               )
             })}
           </VStack>
           <ColumnSplit>
-            {alfabeto.map((letter, i) => {
+            {sortAlfa().map((item, i) => {
               return (
                 <Grid
-                  gridTemplateColumns={['.2fr .8fr']}
+                  gridTemplateColumns={!type ? ['auto .85fr'] : 'auto'}
                   mb={4}
                 >
                   <GridItem
-                    borderTop="1px"
+                    borderTop={!type ? "1px" : '0'}
                     borderColor="blueGreen.400"
                     pt={2}
-                    id={letter}
+                    pb={!type ? 0 : 2}
+                    id={item}
                     as={Heading}
                     colSpan={1}
                     colStart={1}
+                    rowSpan={1}
                     mt="0"
                     mb="auto"
-                    fontSize='4xl'>{letter}</GridItem>
+                    fontSize={!type ? "4xl" : "xl"}
+                  >{item}</GridItem>
                   <GridItem
                     borderTop="1px"
-                    borderColor="blueGreen.400"
+                    borderColor={"blueGreen.400"}
                     pt={2}
-
+                    rowSpan={!type ? 1 : 2}
                     colSpan={1}
-                    colStart={2}
+                    colStart={!type ? 2 : 1}
                     as={List}
                   >
-                    <ListItem
-                      p={2}
-                      borderRadius="lg"
-                      cursor="pointer"
-                      transition="all .2s ease-in-out"
-                      _hover={{
-                        bgColor: 'white.100'
-                      }}
-                    >
-                      <Code>comando-associado</Code> - <ChakraLink fontFamily="body" color="indigoBlue.700" fontWeight="normal" _hover={{ textDecor: 'none' }} >link</ChakraLink>
-                    </ListItem>
-                    <ListItem
-                      p={2}
-                      borderRadius="lg"
-                      cursor="pointer"
-                      transition="all .2s ease-in-out"
-                      _hover={{
-                        bgColor: 'white.100'
-                      }}
-                    >
-                      <Code>comando-associado</Code> - <ChakraLink fontFamily="body" color="indigoBlue.700" fontWeight="normal" _hover={{ textDecor: 'none' }} >link</ChakraLink>
-                    </ListItem>
-                    <ListItem
-                      p={2}
-                      borderRadius="lg"
-                      cursor="pointer"
-                      transition="all .2s ease-in-out"
-                      _hover={{
-                        bgColor: 'white.100'
-                      }}
-                    >
-                      <Code>comando-associado</Code> - <ChakraLink fontFamily="body" color="indigoBlue.700" fontWeight="normal" _hover={{ textDecor: 'none' }} >link</ChakraLink>
-                    </ListItem>
-                    <ListItem
-                      p={2}
-                      borderRadius="lg"
-                      cursor="pointer"
-                      transition="all .2s ease-in-out"
-                      _hover={{
-                        bgColor: 'white.100'
-                      }}
-                    >
-                      <Code>comando-associado</Code> - <ChakraLink fontFamily="body" color="indigoBlue.700" fontWeight="normal" _hover={{ textDecor: 'none' }} >link</ChakraLink>
-                    </ListItem>
-                    <ListItem
-                      p={2}
-                      borderRadius="lg"
-                      cursor="pointer"
-                      transition="all .2s ease-in-out"
-                      _hover={{
-                        bgColor: 'white.100'
-                      }}
-                    >
-                      <Code>comando-associado</Code> - <ChakraLink fontFamily="body" color="indigoBlue.700" fontWeight="normal" _hover={{ textDecor: 'none' }} >link</ChakraLink>
-                    </ListItem>
-                    <ListItem
-                      p={2}
-                      borderRadius="lg"
-                      cursor="pointer"
-                      transition="all .2s ease-in-out"
-                      _hover={{
-                        bgColor: 'white.100'
-                      }}
-                    >
-                      <Code>comando-associado</Code> - <ChakraLink fontFamily="body" color="indigoBlue.700" fontWeight="normal" _hover={{ textDecor: 'none' }} >link</ChakraLink>
-                    </ListItem>
-                    <ListItem
-                      p={2}
-                      borderRadius="lg"
-                      cursor="pointer"
-                      transition="all .2s ease-in-out"
-                      _hover={{
-                        bgColor: 'white.100'
-                      }}
-                    >
-                      <Code>comando-associado</Code> - <ChakraLink fontFamily="body" color="indigoBlue.700" fontWeight="normal" _hover={{ textDecor: 'none' }} >link</ChakraLink>
-                    </ListItem>
-                    <ListItem
-                      p={2}
-                      borderRadius="lg"
-                      cursor="pointer"
-                      transition="all .2s ease-in-out"
-                      _hover={{
-                        bgColor: 'white.100'
-                      }}
-                    >
-                      <Code>comando-associado</Code> - <ChakraLink fontFamily="body" color="indigoBlue.700" fontWeight="normal" _hover={{ textDecor: 'none' }} >link</ChakraLink>
-                    </ListItem>
-                    <ListItem
-                      p={2}
-                      borderRadius="lg"
-                      cursor="pointer"
-                      transition="all .2s ease-in-out"
-                      _hover={{
-                        bgColor: 'white.100'
-                      }}
-                    >
-                      <Code>comando-associado</Code> - <ChakraLink fontFamily="body" color="indigoBlue.700" fontWeight="normal" _hover={{ textDecor: 'none' }} >link</ChakraLink>
-                    </ListItem>
-
+                    <CardLink cmd="next-cmd" link="https://nextjs.org/docs/cmd" />
+                    <CardLink cmd="next-cmd" link="https://nextjs.org/docs/cmd" />
+                    <CardLink cmd="next-cmd" link="https://nextjs.org/docs/cmd" />
+                    <CardLink cmd="next-cmd" link="https://nextjs.org/docs/cmd" />
+                    <CardLink cmd="next-cmd" link="https://nextjs.org/docs/cmd" />
                   </GridItem>
                 </Grid>
               )
